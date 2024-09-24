7c0325c427239dd793797461ffe437f4a69dbe32
Mrpavan777 <115497495+Mrpavan777@users.noreply.github.com>
2024-09-24 14:49:32 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -24,6 +24,20 @@
             $('.navbar').removeClass('sticky-top shadow-sm');
         }
     });
+    // Smooth scrolling
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href');
+        const targetSection = document.querySelector(targetId);
+
+        window.scrollTo({
+            top: targetSection.offsetTop,
+            behavior: 'smooth'
+        });
+    });
+});
 
     
     
