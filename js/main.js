2a5476f6f661ce2d10752a61bd7f8133d41171fd
Mrpavan777 <115497495+Mrpavan777@users.noreply.github.com>
2024-09-24 14:21:33 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -31,24 +31,6 @@ $(window).scroll(function () {
             $('.navbar').fadeOut('slow').css('display', 'none');
         }
     });
-
-
-    // Smooth scrolling on the navbar links
-    $(".navbar-nav a").on('click', function (event) {
-        if (this.hash !== "") {
-            event.preventDefault();
-            
-            $('html, body').animate({
-                scrollTop: $(this.hash).offset().top - 45
-            }, 1500, 'easeInOutExpo');
-            
-            if ($(this).parents('.navbar-nav').length) {
-                $('.navbar-nav .active').removeClass('active');
-                $(this).closest('a').addClass('active');
-            }
-        }
-    });
-    
     
     
     // Dropdown on mouse hover
